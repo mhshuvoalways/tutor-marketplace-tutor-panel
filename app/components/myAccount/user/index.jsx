8985a4403d69a344cc455f1c548e7d56cd8a058a
MH Shuvo <mhshuvoalways@gmail.com>
2024-11-13 09:14:17 +0600

    couldinary

--- app/components/myAccount/user/index.jsx
+++ app/components/myAccount/user/index.jsx
@@ -107,7 +107,7 @@ const Index = () => {
       formData.append("file", image);
       formData.append("upload_preset", "tutor-marketplace");
       const response = await fetch(
-        `https://api.cloudinary.com/v1_1/mhshuvoalways/image/upload`,
+        `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_NAME}/image/upload`,
         {
           method: "POST",
           body: formData,
